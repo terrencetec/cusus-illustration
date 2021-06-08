899ff389bf5f82ad4242d4be50d6f81c503f22ec
lamyc <lamyc627@gmail.com>
2021-06-09 01:18:34 +0800

    get acceleration

--- nano_33/ReadAccelerometer/ReadAccelerometer.ino
+++ nano_33/ReadAccelerometer/ReadAccelerometer.ino
@@ -40,7 +40,7 @@ void setup() {
 void loop() {
 
   if (IMU.accelerationAvailable()) {
-    IMU.readAcceleration(x, y, z);
+    IMU.readAcceleration(x, y, z); //output x, y, z acceleration in terms of g.
 
   }
   
@@ -49,33 +49,5 @@ void loop() {
   Serial.print(y,8);
   Serial.print('\t');
   Serial.println(z,8);
-//  if (x > 0.1) {
-//    x = 100 * x;
-//    degreesX = map(x, 0, 97, 0, 90);
-//    Serial.print("Tilting up ");
-//    Serial.print(degreesX);
-//    Serial.println("  degrees");
-//  }
-//  if (x < -0.1) {
-//    x = 100 * x;
-//    degreesX = map(x, 0, -100, 0, 90);
-//    Serial.print("Tilting down ");
-//    Serial.print(degreesX);
-//    Serial.println("  degrees");
-//  }
-//  if (y > 0.1) {
-//    y = 100 * y;
-//    degreesY = map(y, 0, 97, 0, 90);
-//    Serial.print("Tilting left ");
-//    Serial.print(degreesY);
-//    Serial.println("  degrees");
-//  }
-//  if (y < -0.1) {
-//    y = 100 * y;
-//    degreesY = map(y, 0, -100, 0, 90);
-//    Serial.print("Tilting right ");
-//    Serial.print(degreesY);
-//    Serial.println("  degrees");
-//  }
   delay(100);
 }
